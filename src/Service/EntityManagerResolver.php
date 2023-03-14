365184aac757d29d52023463dcd232661f9f403c
77web <hishida77web@gmail.com>
2023-03-15 00:08:21 +0900

    tryout: implemented EntityManagerResolver

---
+++ src/Service/EntityManagerResolver.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Member;
+use Doctrine\Bundle\DoctrineBundle\ManagerConfigurator;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+class EntityManagerResolver
+{
+    private array $managers = [];
+
+    public function __construct(
+        private readonly EntityManagerInterface $defaultEntityManager,
+        private readonly ManagerConfigurator $managerConfigurator,
+    ) {
+    }
+
+    public function resolve(Member $member): EntityManagerInterface
+    {
+        $key = $member->getUsername();
+        if (!isset($this->managers[$key])) {
+            $defaultConn = $this->defaultEntityManager->getConnection();
+            $conn = DriverManager::getConnection(
+                array_merge($defaultConn->getParams(), ['username' => $key, 'password' => $key]),
+                $defaultConn->getConfiguration(),
+                $defaultConn->getEventManager(),
+            );
+            $em = new EntityManager(
+                $conn,
+                $this->defaultEntityManager->getConfiguration(),
+            );
+            $this->managerConfigurator->configure($em);
+            $this->managers[$key] = $em;
+        }
+
+        return $this->managers[$key];
+    }
+}
